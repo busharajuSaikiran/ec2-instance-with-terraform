abfabcb027e37b869eb121c590baf20d264b26d9
busharajusaikiran <busharajusaikiran@gmail.com>
2024-03-07 15:32:17 +0530

    variables

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+#variable"access-key"
+ #{
+ # default = "AKIAZI2LG4EZKU7TAO5F"
+#}
+
+#variable"Secret-key"
+# {
+ # default = "Y+C2W3/Ku7tvI9E8RjQpw3idcuu7o118Lnw3tsFH"
+#}
+
+variable "ami-os" {
+  type    = string
+  default = "ami-0c7217cdde317cfec"
+}
+
+variable "key" {
+  default = "linux2024"
+}
